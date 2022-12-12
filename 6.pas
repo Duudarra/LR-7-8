0c5101326cf22275b7e69d375ed72bd3179c1d58
Duudarra <117339884+Duudarra@users.noreply.github.com>
2022-12-12 12:38:32 +0300

    Add files via upload

---
+++ 6.pas
@@ -0,0 +1,15 @@
+﻿begin
+var
+s,s_old,s_new: string;
+var s:string; l,k,i:integer;
+begin
+write('Введите строку: ');
+readln(s);
+k:=3;
+for i:=1  to length(s) div 3 do
+begin
+  writeln(s[k]);
+  k+=3;
+  
+end;
+end.
